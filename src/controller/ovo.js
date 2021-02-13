d3ba4712b7cd5355be5be3db45e21126480a9fc2
ncip48 <mbahcip00@gmail.com>
2021-02-13 14:34:22 +0700

    revisi add product pt1

--- src/controller/ovo.js
+++ src/controller/ovo.js
@@ -63,13 +63,28 @@ exports.checkNotif = async (req, res) => {
   }
 };
 
-exports.transferSesama = async (req, res) => {
+exports.checkOvo = async (req, res) => {
   const { token, nominal, tujuan } = req.body;
   try {
     ovoid = new OVOID(token);
     let isOVO = await ovoid.isOVO(nominal, tujuan);
     res.send({
       isOVO,
+    });
+  } catch (err) {
+    res.send({
+      error: err.message,
+    });
+  }
+};
+
+exports.transferSesama = async (req, res) => {
+  const { token, nominal, tujuan, catatan } = req.body;
+  try {
+    ovoid = new OVOID(token);
+    let transferOvo = await ovoid.transferOvo(tujuan, nominal, catatan);
+    res.send({
+      transferOvo,
     });
   } catch (err) {
     res.send({
--- src/routes/router.js
+++ src/routes/router.js
@@ -6,12 +6,14 @@ const {
   checkNotif,
   transferSesama,
   checkOtp,
+  checkOvo,
 } = require("../controller/ovo");
 
 router.get("/ovo/otp", otpOvo);
 router.get("/ovo/auth", checkOtp);
 router.post("/ovo/saldo", checkSaldo);
 router.get("/ovo/notif", checkNotif);
+router.post("/ovo/checkovo", checkOvo);
 router.post("/ovo/transfer", transferSesama);
 
 module.exports = router;
